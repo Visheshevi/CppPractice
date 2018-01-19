3f17fe9cbc346e905ed829e5fa2573d235f8f674
Vishesh <visheshchanana1@gmail.com>
2018-01-18 18:14:51 -0600

    Added node deletion. BUG

--- Trees/BSTInsertion.cpp
+++ Trees/BSTInsertion.cpp
@@ -74,12 +74,88 @@ bool isBalanced(struct node* node){
 	return false;
 }
 
+void deleteNode(struct node* node, int val){
+	struct node* previous = NULL;
+	bool found = false;
+	if(node == NULL){
+		cout<<"Nothing to delete as tree is not yet planted or has been cut to create paper."<<endl;
+	}
+	while(node != NULL){
+		if(node->data == val){
+			found = true;
+			break;
+		}
+		else{
+			previous = node;
+			if(val < node->data)
+				node = node->left;
+			else if(val > node->data)
+				node = node->right;
+		}
+	}
+	if(found == false)
+		cout<<"mangoes do not grow on peepal tree"<<endl;
+	else{
+		if(node->left == NULL && node->right == NULL){
+			if(previous->left == node){
+				previous->left = NULL;
+			}	
+			if(previous->right == node){
+				previous->right = NULL;
+			}
+			delete node;
+		}
+
+		else if(node->left != NULL && node->right == NULL){
+			previous->right = node->left;
+			delete node;
+		}
+		else if(node->left == NULL && node->right != NULL){
+			previous->left = node->right;
+			delete node;
+		}
+//If this comment still present, there is something wrong with the deletion process. Needs to be corrected.
+		else{
+			struct node* check = node->right;
+			if(node->left == NULL && node->right == NULL){
+				node = check;
+				delete check;
+				node->right = NULL;
+			}
+
+			if(node->left->right != NULL){
+				struct node* temp = node->left->right;
+				struct node* prev_temp = node->left;
+				while(temp->right != NULL){
+					prev_temp = temp;
+					temp = temp->right;
+				}
+				node->data = temp->data;
+				delete temp;
+				prev_temp->right = NULL;
+			}
+			else{
+				struct node* temp = node->left;
+				node->data = temp->data;
+				node->right = temp->right;
+				delete temp;
+			}
+
+		}
+	}
+
+	
+}
 
 int main(){
 	int arr[10];
 	int index = 0;
 	int i = 0;
-	struct node* root = newNode(8);
+	int flag;
+	struct node* root;
+	//flag = deleteNode(root,4);
+	//cout<<flag<<endl<<endl;
+	root = newNode(8);
 	root->left = newNode(3);
 	root->left->left = newNode(1);
 	root->right = newNode(10);
@@ -90,16 +166,20 @@ int main(){
 	root->right->right->left = newNode(13);
 	root->right->right->right = newNode(17);
 	root->right->right->right->left = newNode(16);
-	root->right->right->right->left->right = newNode(20);
-	//inorder(root);
+	root->right->right->right->right = newNode(20);
+	//cout<<root->right->right->right->right->right->data<<endl;
+	inorder(root);
+	deleteNode(root,17);
+	inorder(root);
+	
 	//insert(root,5);
 	//inorder(root);
-	if(isBalanced(root))
-		cout<<"isBalanced"<<endl;
-	else
-		cout<<"is not Balanced"<<endl;
+	//if(isBalanced(root))
+	//	cout<<"isBalanced"<<endl;
+	//else
+	//	cout<<"is not Balanced"<<endl;
 	//insert(root,5);
 	//inorder(root);
-	//cout<<search(root,11,0)<<endl;
+	//cout<<search(root,20,0)<<endl;
 	return 0;
 }
